349cae7275cc50f0e53b8bae9053e4fd47bef4ae
nemo <piska.drillka@gmail.com>
2026-04-09 21:29:53 +0300

    feat: assemble auth domain and implement graceful shutdown

--- cmd/gopher-drive/main.go
+++ cmd/gopher-drive/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"GopherDrive/internal/auth"
 	"GopherDrive/internal/infra/config"
+	"GopherDrive/internal/infra/db/postgres"
+	"GopherDrive/internal/infra/db/redis"
 	"GopherDrive/internal/infra/logger"
+	mwLogger "GopherDrive/internal/infra/middleware"
+	sl "GopherDrive/internal/lib/api/logger"
+	"context"
 	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -13,4 +27,68 @@ func main() {
 
 	log.Info("initializing server", slog.String("address", cfg.Address))
 	log.Debug("logger debug mode enabled")
+
+	postgresDB, err := postgres.New(cfg.PostgresURL)
+	if err != nil {
+		log.Error("failed to init postgres DB", sl.SlogErr(err))
+		os.Exit(1)
+	}
+	defer postgresDB.Close()
+
+	redisDB, err := redis.New(cfg.RedisAddr, cfg.RedisPass)
+	if err != nil {
+		log.Error("failed to init redis DB", sl.SlogErr(err))
+		os.Exit(1)
+	}
+	defer func() { _ = redisDB.Close() }()
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(mwLogger.NewLogger(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	authRepository := auth.NewRepository(postgresDB)
+	authService := auth.NewService(authRepository)
+	authHandlers := auth.NewHandler(authService, log)
+
+	router.Route("/user", func(r chi.Router) {
+		r.Post("/register", authHandlers.Register)
+		r.Post("/login", authHandlers.Login)
+	})
+
+	log.Info("server starting", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	go func() {
+		if err = srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server", sl.SlogErr(err))
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("server stopping")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err = srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.SlogErr(err))
+		return
+	}
+
+	log.Info("server stopped")
 }
