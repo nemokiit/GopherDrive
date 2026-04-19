2d8642742d772706b54117cb5a88c10cefe89676
nemo <piska.drillka@gmail.com>
2026-04-19 16:34:59 +0300

    feat(s3): add s3 client initialization with connection check

---
+++ internal/infra/db/s3/s3.go
@@ -0,0 +1,41 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func New(addr, user, password string) (*s3.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	credentialsProvider := credentials.NewStaticCredentialsProvider(user, password, "")
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("us-east-1"),
+		config.WithCredentialsProvider(credentialsProvider),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load s3 config: %w", err)
+	}
+
+	s3Cli := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(addr)
+		o.UsePathStyle = true
+	})
+
+	ctxPing, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	_, err = s3Cli.ListBuckets(ctxPing, &s3.ListBucketsInput{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to s3 (ping): %w", err)
+	}
+
+	return s3Cli, nil
+}
