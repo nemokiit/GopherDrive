51f36c6bb236eaaffcdb2a69cabacd2470f1969b
nemo <piska.drillka@gmail.com>
2026-04-21 16:28:30 +0300

    feat(s3): replace PutObject with UploadObject to support large files and streaming

--- internal/storage/repository/s3.go
+++ internal/storage/repository/s3.go
@@ -7,31 +7,39 @@ import (
 	"io"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 type S3Repository struct {
 	s3Cli      *s3.Client
+	uploader   *transfermanager.Client
 	bucketName string
 }
 
 func NewS3Repo(s3Cli *s3.Client, bucketName string) *S3Repository {
+	uploader := transfermanager.New(s3Cli, func(o *transfermanager.Options) {
+		o.PartSizeBytes = manager.DefaultDownloadPartSize
+		o.Concurrency = manager.DefaultUploadConcurrency
+	})
+
 	return &S3Repository{
 		s3Cli:      s3Cli,
+		uploader:   uploader,
 		bucketName: bucketName,
 	}
 }
 
-func (r *S3Repository) UploadFile(ctx context.Context, key string, reader io.Reader, size int64, contentType string) error {
+func (r *S3Repository) UploadFile(ctx context.Context, key string, reader io.Reader, contentType string) error {
 	const op = "storage.repository.UploadFile"
 
-	_, err := r.s3Cli.PutObject(ctx, &s3.PutObjectInput{
-		Bucket:        aws.String(r.bucketName),
-		Key:           aws.String(key),
-		Body:          reader,
-		ContentLength: aws.Int64(size),
-		ContentType:   aws.String(contentType),
+	_, err := r.uploader.UploadObject(ctx, &transfermanager.UploadObjectInput{
+		Bucket:      aws.String(r.bucketName),
+		Key:         aws.String(key),
+		Body:        reader,
+		ContentType: aws.String(contentType),
 	})
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
